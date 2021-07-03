932226e7250374868cfa9de8abd38b0eab097cf3
berkant <berkantkaya19_03@hotmail.com>
2021-07-03 15:22:17 +0300

    first commit

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -42,7 +42,7 @@ export default class CartTotals extends Component {
                   <strong>$ {cartTotal} </strong>
                 </h5>
                 <PayPalButton
-                  totalAmount={cartTotal}
+                  total={cartTotal}
                   clearCart={clearCart}
                   history={history}
                 />
